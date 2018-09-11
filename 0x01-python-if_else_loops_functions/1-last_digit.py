b9760f9b03705b49cab2fe345aa4c11d15cc3a0a
Alex Farioletti <gsf.nyc@gmail.com>
2018-09-11 13:41:44 -0700

    negs

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 pr_str = "Last digit of {} is {} and is {}"
 if number < 0:
-    rem = (number * -1) % 10
+    rem = -(-number % 10)
 else:
     rem = number % 10
 if rem > 5:
