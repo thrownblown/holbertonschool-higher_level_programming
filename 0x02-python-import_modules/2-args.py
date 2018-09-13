fcc4d813a2405ed09087a66b94207db78f4f22ae
alex farioletti <gsf.nyc@gmail.com>
2018-09-13 12:19:29 -0700

    right nums

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,13 +2,13 @@
 from sys import argv
 
 if __name__ == "__main__":
-    arg_c = len(argv)
-    if arg_c > 1:
-        print("{} arguments:".format(arg_c))
-        for i in range(arg_c):
+    arg_c = len(argv) 
+    if arg_c > 2:
+        print("{} arguments:".format(arg_c - 1))
+        for i in range(1, arg_c):
             print("{}: {}".format(i, argv[i]))
-    elif arg_c == 1:
+    elif arg_c == 2:
         print("{} argument:".format(arg_c))
-        print("1: {}".format(argv[0]))
+        print("1: {}".format(argv[1]))
     else:
         print("0 arguments.")
