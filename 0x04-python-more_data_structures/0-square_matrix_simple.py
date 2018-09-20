623d3cb235fd568415184b9257403ba541566e71
Alex Farioletti <gsf.nyc@gmail.com>
2018-09-20 12:58:38 -0700

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,8 +2,7 @@
 def square_matrix_simple(matrix=[]):
     ret_matrix = []
     for idx, row in enumerate(matrix):
-        if row:
-            ret_matrix.append([])
-            for col in row:
-                ret_matrix[idx].append(col ** 2)
+        ret_matrix.append([])
+        for col in row:
+            ret_matrix[idx].append(col ** 2)
     return(ret_matrix)
