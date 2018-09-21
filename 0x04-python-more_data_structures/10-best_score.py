b0a4a6d4633d7aa2925dec300b4b56dc81b95e34
Alex Farioletti <gsf.nyc@gmail.com>
2018-09-20 21:10:21 -0700

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not len(a_dictionary):
-        return None
-    mx = 0
-    for i in a_dictionary[i]:
-        if a_dictionary[i] > mx:
-            mx = i
-    return mx
+    try:
+        return(max(a_dictionary.values()))
+    except ValueError:
+        return(None)
