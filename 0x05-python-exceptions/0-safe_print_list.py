c74916a3a13aa1ffb960d12a94d6950b638ec364
Alex Farioletti <gsf.nyc@gmail.com>
2018-10-01 17:17:02 -0700

    pep8

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
         idx = 0
         for item in my_list:
             if idx < x:
-                print( item, end='')
+                print(item, end='')
                 idx += 1
             else:
                 break
