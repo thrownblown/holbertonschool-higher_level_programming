ec342bbd4aa37498800892982f704ac1b3f8301a
Alex Farioletti <gsf.nyc@gmail.com>
2018-10-01 17:40:31 -0700

    no indexerror

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+        idx = 0
+        for value in my_list:
+            try:
+                if idx < x:
+                    print("{:d}".format(value))
+                    idx += 1
+                else:
+                    break
+            except (ValueError, TypeError):
+                pass
+        print("")
+        return (idx)
