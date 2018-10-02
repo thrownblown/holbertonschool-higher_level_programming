5cb8ac240027ccb14d6b5bc5c9d43560b9f980b0
Alex Farioletti <gsf.nyc@gmail.com>
2018-10-01 17:16:22 -0700

    init

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        idx = 0
+        for item in my_list:
+            if idx < x:
+                print( item, end='')
+                idx += 1
+            else:
+                break
+        print("")
+        return (idx)
+    except TypeError:
+        return (0)
