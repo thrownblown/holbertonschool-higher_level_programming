e9dc9c70d152ee76cca70366e1da32cf458af1ec
Alex Farioletti <gsf.nyc@gmail.com>
2018-10-02 22:06:01 -0700

    no elifs

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -14,6 +14,6 @@ class Square:
     def size(self, value):
         if value < 0:
             raise ValueError('size must be >= 0')
-        elif type(value) is not int:
+        if type(value) is not int:
             raise TypeError('size must be an integer')
         self.__size = value
--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -14,14 +14,14 @@ class Square:
     def size(self, value):
         if value < 0:
             raise ValueError('size must be >= 0')
-        elif type(value) is not int:
+        if type(value) is not int:
             raise TypeError('size must be an integer')
         self.__size = value
 
         def my_print(self):
             if self.size > 0:
-                for col in range(self.size):
-                    for row in range(self.size):
+                for row in range(self.size):
+                    for col in range(self.size):
                         print('#', end='')
                     print()
             else:
