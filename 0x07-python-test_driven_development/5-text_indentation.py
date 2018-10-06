82e50e8920aaf450d69f432a1668c8179660000d
Alex Farioletti <gsf.nyc@gmail.com>
2018-10-05 21:01:46 -0700

    indent

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def check_types(err, type_list, *argv):
+    ''' if none throws a different err msg, do a separate none check '''
+    for i in argv:
+        if i is None:
+            raise TypeError(err)
+        t = type(i)
+        if t not in type_list:
+            raise TypeError(err)
+    return (True)
+
+
+def text_indentation(txt):
+    check_types("text must be a string", [str], txt)
+    txt = txt.replace("? ", "?").replace("?\n", "?")
+    txt = txt.replace(". ", ".").replace(".\n", ".")
+    txt = txt.replace(": ", ":").replace(":\n", ":")
+    txt = txt.replace(":", ":\n\n").replace("?", "?\n\n").replace(".", ".\n\n")
+    print(txt)
+
+    # for i in range(len(text)):
+    #     if text[i] == "." or text[i] == "?" or text[i] == ":":
+    #         text = "{}{}".format(text[:i + 1], text[i + 1:].strip())
