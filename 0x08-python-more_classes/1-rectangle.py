1d26868c6a5f3dd8ede72a1f91f77e5cd563f00d
Alex Farioletti <gsf.nyc@gmail.com>
2018-10-08 19:43:38 -0700

    where that file go

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Module with the Rectable class"""
+
+
+class Rectangle:
+    """Rectangle class with height and width attributes"""
+    def __init__(self, width=0, height=0):
+        """Initer"""
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Returns private attr width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets private attr width"""
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__size = value
+
+    @property
+    def height(self):
+        """Returns private attr height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets private attr height"""
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
