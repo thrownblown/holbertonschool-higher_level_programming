5fc71dd09b6fd3f1d599d1dccda354f404579307
Alex Farioletti <gsf.nyc@gmail.com>
2018-10-08 21:06:15 -0700

    __size

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -8,6 +8,10 @@ class Rectangle:
         """Initer"""
         self.__width = width
         self.__height = height
+        Rectangle.number_of_instances += 1
+
+    number_of_instances = 0
+    print_symbol = '#'
 
     @property
     def width(self):
@@ -46,3 +50,33 @@ class Rectangle:
         if self.height is 0 or self.width is 0:
             return(0)
         return(2 * (self.width + self.height))
+
+    def __str__(self):
+        """Returns str of # with instance dimensions"""
+        rect = ''
+        if self.height is 0 or self.width is 0:
+            return(rect)
+        for row in range(self.height):
+            for col in range(self.width):
+                rect += str(self.print_symbol)
+            if row != self.height - 1:
+                rect += '\n'
+        return(rect)
+
+    def __repr__(self):
+        """Returns a string representation of the rectangle"""
+        return("Rectangle({}, {})".format(self.width, self.height))
+
+    def __del__(self):
+        """Prints epitaph for del rect"""
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle... ")
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """Returns the greater of two rects by area """
+        if type(rect_1) is not Rectangle:
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        elif type(rect_2) is not Rectangle:
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        return (rect_1 if rect_1.area() >= rect_2.area() else rect_2)
