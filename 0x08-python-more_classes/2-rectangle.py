6593dd6f9126ed62c1694dc67e2c3f0f20a626b8
Alex Farioletti <gsf.nyc@gmail.com>
2018-10-08 19:48:27 -0700

    docstrings

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
+"""Module with the Rectable class"""
+
+
 class Rectangle:
+    """Rectangle class with height and width attributes"""
     def __init__(self, width=0, height=0):
+        """Initer"""
         self.__width = width
         self.__height = height
 
     @property
     def width(self):
+        """Returns private attr width"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Sets private attr width"""
         if type(value) is not int:
             raise TypeError('width must be an integer')
         if value < 0:
@@ -18,10 +25,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """Returns private attr height"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Sets private attr height"""
         if type(value) is not int:
             raise TypeError('height must be an integer')
         if value < 0:
@@ -29,9 +38,11 @@ class Rectangle:
         self.__height = value
 
     def area(self):
+        """Returns instance area"""
         return(self.width * self.height)
 
     def perimeter(self):
+        """Returns instance perimeter"""
         if self.height is 0 or self.width is 0:
             return(0)
         return(2 * (self.width + self.height))
