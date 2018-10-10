c439416ffc7484cca96f7004bf82c0e0c26a9023
alex farioletti <gsf.nyc@gmail.com>
2018-10-10 11:13:27 -0700

    elif

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -23,7 +23,7 @@ class Rectangle:
         """Sets private attr width"""
         if type(value) is not int:
             raise TypeError('width must be an integer')
-        if value < 0:
+        elif value < 0:
             raise ValueError('width must be >= 0')
         self.__width = value
 
