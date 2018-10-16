1e853f2cffa26f65443152843ce984353c0ce064
Alex Farioletti <gsf.nyc@gmail.com>
2018-10-16 09:50:57 -0700

    giveem

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -40,6 +40,7 @@ class Rectangle(BaseGeometry):
 class Square(Rectangle):
     ''' shape class for 4 sided object with right angles and equal sides '''
     def __init__(self, size):
+        self.integer_validator("size", size)
         super(Square, self).__init__(size, size)
         self.__size = size
 
