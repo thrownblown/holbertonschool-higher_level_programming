7807d693f38969b66c01220346c7c158ae8f3a25
Alex Farioletti <gsf.nyc@gmail.com>
2018-10-16 23:50:17 -0700

    chmods

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -6,7 +6,7 @@ class Student:
     """ Student """
 
     def __init__(self, first_name, last_name, age):
-        """ initer """
+	    """ returns Student dict """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
