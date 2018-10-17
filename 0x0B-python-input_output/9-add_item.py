32a1e8b399d7c2cf81895ea8155d61e7e8be5fc0
Alex Farioletti <gsf.nyc@gmail.com>
2018-10-16 22:15:52 -0700

    appends

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """ appends args to list, and writes list to file """
 from sys import argv
-save = __import__('7-save_to_json_file').save_to_json_file
-load = __import__('8-load_from_json_file').load_from_json_file
+save_json = __import__('7-save_to_json_file').save_to_json_file
+load_json = __import__('8-load_from_json_file').load_from_json_file
 
 
 if __name__ == "__main__":
     try:
-        list_from_file = load("add_item.json")
+        list_from_file = load_json("add_item.json")
         list_from_file.extend([e for e in argv[1:]])
     except FileNotFoundError:
         list_from_file = [e for e in argv[1:]]
-    finally:
-        save(list_from_file, "add_item.json")
+    save_json(list_from_file, "add_item.json")
