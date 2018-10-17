002e4a95f1b3f7ce624d2dfd6cfd19f238cc1153
Alex Farioletti <gsf.nyc@gmail.com>
2018-10-16 23:54:16 -0700

    keyval

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Student class module """
+
+
+class Student:
+    """ Student """
+
+    def __init__(self, first_name, last_name, age):
+        """ initer """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ returns Student dict """
+        if attrs is None:
+            return self.__dict__
+        elif type(attrs) is list:
+            obj = {}
+            for key, val in self.__dict__.items():
+                if key in attrs:
+                    obj[key] = val
+            return obj
+
+    def reload_from_json(self, json):
+        """ sets attributes from json dict """
+        for key, val in json.items():
+            self.__dict__[key] = val
