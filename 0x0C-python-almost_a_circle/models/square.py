b8a52b31b2216307ae9557af597652e66f3806d3
Alex Farioletti <gsf.nyc@gmail.com>
2018-10-22 14:47:42 -0700

    hip to be square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-''' Square Class module '''
-from .Rectangle import Rectangle
+""" Square Class module """
+from .rectangle import Rectangle
 
 
 class Square(Rectangle):
-    ''' shape class for 4 sided object with right angles and equal sides '''
-    def __init__(self, size):
-        self.integer_validator("size", size)
-        super(Square, self).__init__(size, size)
+    """ shape class for 4 sided obj with and equal sides """
+    def __init__(self, size, x=0, y=0, id=None):
+        super(Square, self).__init__(size, size, x=x, y=y, id=id)
