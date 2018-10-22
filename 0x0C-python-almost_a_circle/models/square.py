3e2f9e5210165d02a281d157a6423b0dff994dc3
Alex Farioletti <gsf.nyc@gmail.com>
2018-10-22 14:50:31 -0700

    nokwarg

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,4 +6,4 @@ from .rectangle import Rectangle
 class Square(Rectangle):
     """ shape class for 4 sided obj with and equal sides """
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(size, size, x=x, y=y, id=id)
+        super().__init__(size, size, x, y, id)
