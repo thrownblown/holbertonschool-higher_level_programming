e4884c3a140a4a851fffba540524f381bced41f7
Alex Farioletti <gsf.nyc@gmail.com>
2018-10-22 15:00:12 -0700

    size setter

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,3 +8,13 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
         self.size = size
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+        self.__size = value
