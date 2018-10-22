ee65b568841e690108a7d9f4d1d0ac197a4c9dbd
Alex Farioletti <gsf.nyc@gmail.com>
2018-10-22 14:55:08 -0700

    size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,3 +7,4 @@ class Square(Rectangle):
     """ shape class for 4 sided obj with and equal sides """
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
+        self.size = size
