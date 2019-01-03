b6a1b568fc15f728534fc0b75a912ddd65074a02
Alex Farioletti <gsf.nyc@gmail.com>
2019-01-03 21:42:53 +0000

    tripple tuplr

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""prints all the states in db"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    cur = db.cursor()
+    cur.execute(
+        """
+        SELECT cities.id, cities.name, states.name
+        FROM cities, states
+        WHERE cities.state_id=states.id
+        ORDER BY id ASC;
+        """
+    )
+
+    for state in cur.fetchall():
+        print(state)
+    db.close()
