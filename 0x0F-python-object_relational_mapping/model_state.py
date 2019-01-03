a98ce53197b9b387721dbf065d7ef5f554a7511d
Alex Farioletti <gsf.nyc@gmail.com>
2019-01-03 22:34:42 +0000

    decruft

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """makin states alchemiclly"""
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, Integer, String
 
 
 Base = declarative_base()
@@ -10,5 +10,4 @@ Base = declarative_base()
 class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
-    name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'))
+    name = Column(String(128), nullable=False))
