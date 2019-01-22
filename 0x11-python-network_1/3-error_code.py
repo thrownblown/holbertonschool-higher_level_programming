f48e50a40ca803f4a59df1a1aeb410074b8740d8
Alex Farioletti <gsf.nyc@gmail.com>
2019-01-21 22:40:49 -0800

    flossin

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     try:
         with urllib.request.urlopen(sys.argv[1]) as response:
             html = response.read()
-            print(html)
+            print(html.decode("utf-8"))
     except urllib.error.HTTPError as err:
         print("Error code: {}".format(err.code))
---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Python script that fetches https://intranet.hbtn.io/status
+import requests
+
+
+if __name__ == "__main__":
+    html = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(html.text)))
+    print("\t- content: {}".format(html.text))
