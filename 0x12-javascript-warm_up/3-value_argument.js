cb614d2ca91ab02d0ae76de1e68e9a34f4876806
Alex Farioletti <gsf.nyc@gmail.com>
2019-02-12 22:29:18 -0800

    fix

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 if (!process.argv[2]) {
   console.log('No arguments');
-} else if (process.argv[3]) {
-  console.log(process.argv[3]);
+} else {
+  console.log(process.argv[2]);
 }
