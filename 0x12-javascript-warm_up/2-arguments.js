2efe4ffea7a56ebd697c3ead26499a125a584d6c
Alex Farioletti <gsf.nyc@gmail.com>
2019-02-12 22:09:12 -0800

    delooped

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -6,6 +6,3 @@ if (process.argv.length === 2) {
 } else if (process.argv.length > 3) {
   console.log('Arguments found');
 }
-for (let j = 0; j < process.argv.length; j++) {
-  console.log(j + ' -> ' + (process.argv[j]));
-}
