5cc2f194e02c6225c7c9ec31fc31948442dfcb93
Alex Farioletti <gsf.nyc@gmail.com>
2019-02-12 23:03:58 -0800

    no arrow funciness

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 let fact = process.argv[2];
 
-let factorial = (a) => {
+function factorial (a) {
   if (a === 0 || a === 1) {
     return 1;
   } else {
     return a * factorial(a - 1);
   }
-};
+}
 
 if (isNaN(fact)) {
   console.log(1);
