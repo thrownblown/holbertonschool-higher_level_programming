8fd9115f80fe0cb7b69921e4cb11d38799f87ede
Alex Farioletti <gsf.nyc@gmail.com>
2019-02-12 22:24:09 -0800

    + is overloaded

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,2 @@
 #!/usr/bin/node
-if (!process.argv[2]) {
-  console.log('No arguments');
-} else if (process.argv[2]) {
-  console.log(process.argv[2]);
-}
+console.log(process.argv[2] + ' is ' + process.argv[3]);
