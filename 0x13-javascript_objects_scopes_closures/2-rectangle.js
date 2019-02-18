9e10443fd43f8636bc3a69910c57a511dffd41cb
Alex Farioletti <gsf.nyc@gmail.com>
2019-02-18 12:42:04 -0800

    empty

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -4,9 +4,11 @@ class Rectangle {
   // emppty rectangle class
   constructor (w, h) {
     // no longer useless
-    this.width = w;
-    this.height = h;
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
   }
 }
 
-module.exports.Rectangle = Rectangle;
+module.exports = Rectangle;
