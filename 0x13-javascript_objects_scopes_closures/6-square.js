9de6abae9a17e51a5e9bcb83e9c5ad4bc1949220
Alex Farioletti <gsf.nyc@gmail.com>
2019-02-18 14:13:40 -0800

    what req fail?

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 class Rectangle {
-  // emppty rectangle class
+  // non empty rectangle class
   constructor (w, h) {
     // no longer useless
     if (w > 0 && h > 0) {
@@ -20,19 +20,25 @@ class Rectangle {
       console.log(x);
     }
   }
+
   rotate () {
     // that exchanges the width and the height of the rectangle
     let temp = this.height;
     this.height = this.width;
     this.width = temp;
   }
+
   double () {
     // that multiples the width and the height of the
     this.height = this.height * 2;
     this.width = this.width * 2;
   }
+
   charPrint (c) {
     // prints the rectangle using the character X
+    if (c === 0) {
+      c = '0';
+    }
     for (let row = 0; row < this.height; row++) {
       let x = '';
       for (let col = 0; col < this.width; col++) {
