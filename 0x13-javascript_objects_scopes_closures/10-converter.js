4a89467da75b57b21f6f926964d4b9cd2f730e03
Alex Farioletti <gsf.nyc@gmail.com>
2019-02-18 14:08:16 -0800

    inscrutable

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,6 +1,2 @@
 #!/usr/bin/node
-exports.converter = function (base) {
-  return function (num) {
-    return num.toString(base);
-  };
-};
+exports.converter = (base) => { return (num) => num.toString(base); };
