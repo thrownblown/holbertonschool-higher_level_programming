cb10ff2e15eacdba1713fde168d8931c3e894f1b
Alex Farioletti <gsf.nyc@gmail.com>
2019-02-19 14:30:23 -0800

    only 18

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,6 +4,7 @@ const request = require('request');
 request(process.argv[2], (err, res, body) => {
   if (err) { return console.log(err); }
   let bdy = JSON.parse(body);
-  const reducer = (acc, item) => acc + item.characters.includes('https://swapi.co/api/people/18/');
+  const includer = (acc, item) => acc + item.includes('18/');
+  const reducer = (acc, item) => acc + item.characters.reduce(includer, 0);
   console.log(bdy.results.reduce(reducer, 0));
 });
