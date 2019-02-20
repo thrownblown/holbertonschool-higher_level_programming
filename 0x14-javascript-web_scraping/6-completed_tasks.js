1eeef53354b10e22a58d3ed7c4e4ac9637a33184
Alex Farioletti <gsf.nyc@gmail.com>
2019-02-19 21:38:21 -0800

    completed

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,6 +4,10 @@ const request = require('request');
 request(process.argv[2], (err, res, body) => {
   if (err) { return console.log(err); }
   let ret = {};
-  JSON.parse(body).forEach(item => { ret[item.userId] ? ret[item.userId]++ : ret[item.userId] = 1; });
+  JSON.parse(body).forEach(item => {
+    if (item.completed) {
+      ret[item.userId] ? ret[item.userId] += 1 : ret[item.userId] = 1;
+    }
+  });
   console.log(ret);
 });
