a2e6476a2132cea9f8247d027c77b82de23e4a7a
Alex Farioletti <gsf.nyc@gmail.com>
2019-02-19 21:08:29 -0800

    utf8

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -4,9 +4,7 @@ const request = require('request');
 
 request(process.argv[2], (err, res, body) => {
   if (err) { return console.log(err); }
-  fileSys.writeFile(process.argv[3], body, (err) => {
+  fileSys.writeFile(process.argv[3], body, 'utf8', (err) => {
     if (err) console.log(err);
   });
 });
-
-
